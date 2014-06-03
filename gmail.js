b232a494ff1ff0ea748af35a929793eb162b55c5
robnagler <github>
2014-06-02 19:06:00 -0600

    removed logging

--- gmail.js
+++ gmail.js
@@ -6,24 +6,18 @@
     }
     var range = sel.getRangeAt(0);
     var old = sel.toString();
-    console.log(old);
-    if (old.indexOf('<') >= 0) {
-      alert('Remove formatting before selecting text');
-      return;
-    }
     range.deleteContents();
     range.insertNode(
       range.createContextualFragment(
 	            '<a href="https://www.google.com/search?q='
 	    + encodeURIComponent(old)
 	    + '">' + old + "</a>"));
-    return;
   };
   var keyClick = function(evt) {
     if (!document.hasFocus()) {
       return true;
     }
-    if (!evt.ctrlKey && !evt.shiftKey && evt.altKey && evt.keyCode == 76) {
+    if (!evt.ctrlKey && !evt.shiftKey && !evt.metaKey && evt.altKey && evt.keyCode == 76) {
       replaceSelection();
       evt.stopPropagation();
       evt.preventDefault();
