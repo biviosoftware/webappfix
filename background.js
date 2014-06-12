28384fc7dbce0d3b1b87155c643c7c651300e958
robnagler <github@q33.us>
2014-06-12 06:52:18 -0600

    checkpoint of ctmone*

--- background.js
+++ background.js
@@ -1,7 +1,16 @@
 (function(){
+  function t(str) {
+    console.log(str);
+  }
   function _host(url) {
     var host = url.attr('host');
+    t(host);
     if (host != '') {
+      if (host == 'www.ctmone.com') {
+        if (/ContactSheet.asp/.test(url.attr('path'))) {
+	  return 'ctmone-contactsheet';
+	}
+      }
       return host;
     }
     if (url.attr('protocol') != 'file') {
@@ -11,6 +20,7 @@
     if (!found) {
       return '';
     }
+    t(found[1]);
     return found[1];
   }
   chrome.tabs.onUpdated.addListener(function(tabId, changeInfo, tab) {
@@ -19,6 +29,13 @@
       chrome.pageAction.setPopup({
 	tabId: tabId,
 	popup: 'gmail-popup.html'
+      });
+      chrome.pageAction.show(tabId);
+      break;
+    case 'ctmone-contactsheet':
+      chrome.pageAction.setPopup({
+	tabId: tabId,
+	popup: 'ctmone-popup.html'
       });
       chrome.pageAction.show(tabId);
       break;
